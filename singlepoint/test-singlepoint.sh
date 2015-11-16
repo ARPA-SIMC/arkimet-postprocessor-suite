9103ce350fce3bfd701d3517d5cdf2358a44ddd7
Emanuele Di Giacomo <edigiacomo@arpa.emr.it>
2015-11-16 14:04:31 +0100

    removed tabs

--- singlepoint/test-singlepoint.sh
+++ singlepoint/test-singlepoint.sh
@@ -7,38 +7,38 @@ outfile2=$(mktemp)
 
 cleanup()
 {
-	rm -f $outfile1 $outfile2
+    rm -f $outfile1 $outfile2
 }
 
 trap cleanup EXIT
 
 test_1()
 {
-	arki-scan --inline ${srcdir}/test.grib1 | ${srcdir}/singlepoint 10 2>/dev/null && return 1 || return 0
+    arki-scan --inline ${srcdir}/test.grib1 | ${srcdir}/singlepoint 10 2>/dev/null && return 1 || return 0
 }
 test_2()
 {
-	arki-scan --inline ${srcdir}/test.grib1 | ${srcdir}/singlepoint 23 43 > $outfile1 || return 1
-	[[ -s $outfile1 ]] || return 1
-	dbamsg dump --interpreted --csv $outfile1 | awk -F, '{ if ($12 == "B05001" || $12 == "B06001") { if (($12 == "B05001" && $13 != 43) || ($12 == "B06001" && $13 != 23)) { print ; exit 1 } } }' || return 1
+    arki-scan --inline ${srcdir}/test.grib1 | ${srcdir}/singlepoint 23 43 > $outfile1 || return 1
+    [[ -s $outfile1 ]] || return 1
+    dbamsg dump --interpreted --csv $outfile1 | awk -F, '{ if ($12 == "B05001" || $12 == "B06001") { if (($12 == "B05001" && $13 != 43) || ($12 == "B06001" && $13 != 23)) { print ; exit 1 } } }' || return 1
 }
 test_3()
 {
-	arki-scan --inline ${srcdir}/test.grib1 | ${srcdir}/singlepoint 60 80 > $outfile1 || return 1
-	dbamsg dump --interpreted --csv $outfile1 | awk -F, '/^60,80/{ if ($4 != "") { print ; exit 1 }}' || return 1
+    arki-scan --inline ${srcdir}/test.grib1 | ${srcdir}/singlepoint 60 80 > $outfile1 || return 1
+    dbamsg dump --interpreted --csv $outfile1 | awk -F, '/^60,80/{ if ($4 != "") { print ; exit 1 }}' || return 1
 }
 
 numok=0
 numko=0
 for (( i = 1 ; i <= 3 ; ++i ))
 do
-	if test_$i
-	then
-		(( numok++ ))
-	else
-		echo "Failed test_$i"
-		(( numko++ ))
-	fi
+    if test_$i
+    then
+        (( numok++ ))
+    else
+        echo "Failed test_$i"
+        (( numko++ ))
+    fi
 done
 
 echo "Success: $numok, Failed: $numko"
--- subarea/test-subarea.sh
+++ subarea/test-subarea.sh
@@ -7,41 +7,41 @@ outfile2=$(mktemp)
 
 cleanup()
 {
-	rm -f $outfile1 $outfile2
+    rm -f $outfile1 $outfile2
 }
 
 trap cleanup EXIT
 
 test_1()
 {
-	arki-query --inline '' $srcdir/test.grib1 | $srcdir/subarea 10 2>/dev/null && return 1 || return 0
+    arki-query --inline '' $srcdir/test.grib1 | $srcdir/subarea 10 2>/dev/null && return 1 || return 0
 }
 test_2()
 {
-	arki-query --inline '' $srcdir/test.grib1 | $srcdir/subarea 10 20 2>/dev/null && return 1 || return 0
+    arki-query --inline '' $srcdir/test.grib1 | $srcdir/subarea 10 20 2>/dev/null && return 1 || return 0
 }
 test_3()
 {
-	arki-query --inline '' $srcdir/test.grib1 | $srcdir/subarea 10 20 30 2>/dev/null && return 1 || return 0
+    arki-query --inline '' $srcdir/test.grib1 | $srcdir/subarea 10 20 30 2>/dev/null && return 1 || return 0
 }
 test_4()
 {
-	arki-query --inline '' $srcdir/test.grib1 | $srcdir/subarea 10 20 30 40 > $outfile1 || return 1
-	[[ -s $outfile1 ]] || return 1
-	grib_get_data $outfile1 | awk '/Latitude.*/{} /^[ \t]*[0-9]/{ lat=$1; lon=$2 ; if (lon < 9 || lon > 31 || lat < 19 || lat > 41 ) { print ; exit 1 ; } }' || return 1
+    arki-query --inline '' $srcdir/test.grib1 | $srcdir/subarea 10 20 30 40 > $outfile1 || return 1
+    [[ -s $outfile1 ]] || return 1
+    grib_get_data $outfile1 | awk '/Latitude.*/{} /^[ \t]*[0-9]/{ lat=$1; lon=$2 ; if (lon < 9 || lon > 31 || lat < 19 || lat > 41 ) { print ; exit 1 ; } }' || return 1
 }
 
 numok=0
 numko=0
 for (( i = 1 ; i <= 4 ; ++i ))
 do
-	if test_$i
-	then
-		(( numok++ ))
-	else
-		echo "Failed test_$i"
-		(( numko++ ))
-	fi
+    if test_$i
+    then
+        (( numok++ ))
+    else
+        echo "Failed test_$i"
+        (( numko++ ))
+    fi
 done
 
 echo "Success: $numok, Failed: $numko"
